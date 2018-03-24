f3e10cfc223b1672323fa70cb0c77e5f02fbb9dc
jinyanliu <jinyan.liu@truecaller.com>
2018-03-24 12:50:56 +0100

    Populate placeOfOrigin and description.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -12,8 +13,8 @@ import com.udacity.sandwichclub.utils.JsonUtils;
 
 public class DetailActivity extends AppCompatActivity {
 
-    public static final String EXTRA_POSITION = "extra_position";
-    private static final int DEFAULT_POSITION = -1;
+    public static final  String EXTRA_POSITION   = "extra_position";
+    private static final int    DEFAULT_POSITION = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +44,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +57,13 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView placeOfOrigin = findViewById(R.id.place_of_origin_tv);
+        String placeOfOriginText = sandwich.getPlaceOfOrigin();
+        if (placeOfOriginText.isEmpty()) placeOfOriginText = "Unknown";
+        placeOfOrigin.setText(placeOfOriginText);
 
+        TextView description = findViewById(R.id.description_tv);
+        description.setText(sandwich.getDescription());
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,48 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    private static final String LOG_TAG = JsonUtils.class.getSimpleName();
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONObject nameJSONObject = jsonObject.getJSONObject("name");
+            String mainName = nameJSONObject.getString("mainName");
+            JSONArray alsoKnownAsNameJSONArray = nameJSONObject.getJSONArray("alsoKnownAs");
+
+            List<String> alsoKnownAs = new ArrayList<>();
+            for (int i = 0; i < alsoKnownAsNameJSONArray.length(); i++) {
+                alsoKnownAs.add(alsoKnownAsNameJSONArray.getString(i));
+            }
+
+            String placeOfOrigin = jsonObject.getString("placeOfOrigin");
+            String description = jsonObject.getString("description");
+            String image = jsonObject.getString("image");
+            JSONArray ingredientsJSONArray = jsonObject.getJSONArray("ingredients");
+
+            List<String> ingredients = new ArrayList<>();
+            for (int i = 0; i < ingredientsJSONArray.length(); i++) {
+                ingredients.add(ingredientsJSONArray.getString(i));
+            }
+
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Failed to pass Sandwich JSON.");
+            return null;
+        }
     }
 }
